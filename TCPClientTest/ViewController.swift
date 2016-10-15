fd9c492292580322cb92f4eeb76374295f4b9d37
浅井悠佑 <yu_yu_jp@ezweb.ne.jp>
2016-10-15 10:57:54 +0900

    Initial Commit

---
+++ TCPClientTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TCPClientTest
+//
+//  Created by Yusuke Asai on 2016/10/15.
+//  Copyright © 2016年 Yusuke Asai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TCPClientTestTests/TCPClientTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  TCPClientTestTests.swift
+//  TCPClientTestTests
+//
+//  Created by Yusuke Asai on 2016/10/15.
+//  Copyright © 2016年 Yusuke Asai. All rights reserved.
+//
+
+import XCTest
+@testable import TCPClientTest
+
+class TCPClientTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
